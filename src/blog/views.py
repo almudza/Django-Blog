82b4d067d6bba268ef629081ee76f843dc31f39f
zifza <alifulmudzakir@gmail.com>
2017-06-08 04:14:49 +0700

    add similar posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -15,6 +15,8 @@ from django.core.mail import send_mail
 # Taggit view
 from taggit.models import Tag
 
+from django.db.models import Count
+
 
 
 
@@ -77,8 +79,13 @@ def post_detail(request, year, month, day, post) :
 	else:
 		comment_form = CommentForm()
 
+	# List similar posts
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
 	return render(request, 
-				'blog/post/detail.html', {'post': post, 'comments':comments, 'comment_form': comment_form})
+				'blog/post/detail.html', {'post': post, 'comments':comments, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 # Email forms from forms.py
